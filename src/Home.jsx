4fe9f3f5f0fb9f9ad9ea9da1a03f1c7c6e4fc5d9
sguborges <sguborges@gmail.com>
2025-05-05 12:28:07 -0300

    pokeman

---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+import poke from "./assets/pokeapi_256.png"
+const Home = () => {
+    return (
+        <div className="pokeflex">
+            <img src={poke}/>
+            <img src={poke}/>
+            <img src={poke}/>
+            <img src={poke}/>
+            <img src={poke}/>
+            <img src={poke}/>
+        </div>
+    )
+}
+
+export default Home
