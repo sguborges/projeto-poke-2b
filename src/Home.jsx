a43e94baed47272c939c00613ccdc2a76d680ba2
sguborges <128099016+sguborges@users.noreply.github.com>
2025-06-04 14:46:35 +0000

    pokimon

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,22 +1,13 @@
 import poke from "./assets/pokeapi_256.png"
 import eevee from "./assets/eevee.jpg"
 import './Home.css'
-
+import PokeFlex from "./PokeFlex"
+import PokeGrid from "./PokeGrid"
 const Home = () => {
     return (
-        <div className="pokeflex">
-            <img src={poke}/>
-            <img src={eevee}/>
-            <img src={poke}/>
-            <img src={eevee}/>
-            <img src={poke}/>
-            <img src={eevee}/>
-            <img src={poke}/>
-            <img src={eevee}/>
-            <img src={poke}/>
-            <img src={eevee}/>
-            <img src={poke}/>
-            <img src={eevee}/>
+        <div className="Home">
+            <PokeFlex />
+            <PokeGrid />
         </div>
     )
 }
